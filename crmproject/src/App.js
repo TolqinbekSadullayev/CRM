7b17ec120eea1aadd76155057e888f60fc6039d0
TolqinbekSadullayev <9527125st@gmail.com>
2023-04-27 14:20:13 +0500

    kichik bag tuzatildi...

--- crmproject/src/App.js
+++ crmproject/src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
-import Navbar from './Components/Home/Navbar';
-import { Outlet } from 'react-router-dom';
-import Navbartop from './Components/Home/Navbartop';
+import "./App.css";
+import Navbar from "./Components/Home/Navbar";
+import { Outlet } from "react-router-dom";
+import Navbartop from "./Components/Home/Navbartop";
 
 function App() {
   return (
     <div className="App">
-      <Navbartop/>
-      <div className='row'>
-        <div className='col-3'>
-        <Navbar/>
-      <Outlet />
+      <Navbartop />
+      <div className="row">
+        <div className="col-3">
+          <Navbar />
         </div>
-        <div className='col-9 bgg'>
-        
+        <div className="col-9 bgg">
+          <Outlet />
+
         </div>
       </div>
     </div>
